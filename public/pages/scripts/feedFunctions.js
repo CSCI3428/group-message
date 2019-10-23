cd562f4047735f96768c9a251a8ecfdfcf6e8fcb
Patrick <pat6851@gmail.com>
2019-10-23 11:20:38 -0300

    Gotta commit something

--- public/pages/scripts/feedFunctions.js
+++ public/pages/scripts/feedFunctions.js
@@ -1,10 +1,31 @@
 
-function display(){
+function send(){
     
+    // Variable Declaration
+    var li = document.createElement("li");
     var message = document.getElementById("msg").value;
+    var t = document.createTextNode(message);
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
     
-    document.getElementById("chat").innerHTML = message;
+    li.appendChild(t);
     
-    document.getElementById("msg").innerHTML = "";
+    if(message !== ""){
+        document.getElementById("myList").appendChild(li);
+    }
     
-}
+    document.getElementById("message").value = "";
+
+   
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+        }
+    }
+    
+} // END send function
