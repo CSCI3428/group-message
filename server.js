94afda43dcdb073043e90e855784558a10255930
Rebecca <rebecca.herritt@hotmail.com>
2019-10-26 20:14:43 -0300

    Hi

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express');
+var server = express();
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
 
 
-var server = http.createServer(function(req,res) {
-console.log(req.url);
-res.writeHead(200, {'Content-Type' : 'text/html'});
-var stream = fs.createReadStream(__dirname + '/frontPage.html', 'utf8');
-stream.pipe(res);
-});
+server.use('/scripts', express.static(__dirname + '/scripts'));
+server.use('/css', express.static(__dirname + '/css'));
+server.use(express.static(__dirname));
+
+var allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+};
+server.use(allowCrossDomain);
 
 server.listen(3001, function () {
     console.log('Listening on port 3001');
